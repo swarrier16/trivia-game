cb21cfe6d0088f7ab0d138b118d713f295007433
swarrier16 <swarrier16@gmail.com>
2016-04-12 09:45:58 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// Trivia game Psuedo Code
+
+// Welcome Page with msg "Welcome to Disney Trivia"
+// Directions?
+// Three strikes and you lose.
+// The user should type their name.
+// When user clicks button to submit their name the first question appears
+
+$(document).ready(function() {
+
+  var playerName = '';
+
+  // Store player name
+  $('#name-button').on('click', function(e) {
+    e.preventDefault(); // prevents form from submitting to a database
+    playerName = $('#player-name').val();
+    $('#show-player-name').text(playerName);
+  })
+
+}) // ready function ends
